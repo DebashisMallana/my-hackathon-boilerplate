e9ac4e011ea0ad8eb510d0e51f3d523580846d16
DebashisMallana <debashismallana12@gmail.com>
2026-09-09 22:31:45 +0530

    chore: initial boilerplate setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working! 🎉</h1>
+    </div>
+  )
+}
+
+export default App
